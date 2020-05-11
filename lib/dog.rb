1dfd9da2e5217e2ade11fe19480b1f8b727ff206
Vianka Rivera <viankalrivera@gmail.com>
2020-05-11 02:06:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,6 +3,14 @@ class Dog
   def bark
     puts "Woof!"
   end
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+end 
 end
 fido = Dog.new
 fido.name = "Fido"
